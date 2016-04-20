a110a84250e0b223715a7162282be1670a095c90
Alisson Gomes <alisson.php@gmail.com>
2016-04-20 08:14:25 -0300

    página de pesquisa

--- resources/views/site/pesquisa.blade.php
+++ resources/views/site/pesquisa.blade.php
@@ -36,10 +36,39 @@
         </div>
     </nav>
 </div>
-<div class="container-fluid grey lighten-4">
+<div class="container-fluid grey lighten-4 fixed">
     <div class="container">
         <div class="row">
-            #pesquisa
+            <div class="input-field col m4">
+                <select multiple>
+                    <option value="" disabled selected>especialidades</option>
+                    <option value="1">Massas</option>
+                    <option value="2">Grelhados</option>
+                    <option value="3">Carnes</option>
+                </select>
+            </div>
+            <div class="input-field col m4">
+                <select multiple>
+                    <option value="" disabled selected>formas de pagamento</option>
+                    <option value="1">Cartão - Débito</option>
+                    <option value="2">Cartão - Crédito</option>
+                    <option value="2">Cartão - Ticket</option>
+                    <option value="3">Dinheiro</option>
+                </select>
+            </div>
+            <div class="col m4">
+                <p>Bairro de Entrega:</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="input-field col m12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">Pesquisar restaurante ... Ex.: Casa de Massas, CIA da Carne, etc.</label>
+            </div>
         </div>
     </div>
 </div>
@@ -49,12 +78,12 @@
         <div class="row">
             <div class="col s12">
                 <h3><i class="mdi-content-send brown-text"></i></h3>
-                <h4>Restaurantes Abertos</h4>
+                <h4>Restaurantes Abertos <div class="chip green white-text">8</div></h4>
             </div>
         </div>
 
         <div class="row">
-            <div class="col m3">
+            <div class="col l3 m3 s12">
                 <div class="card hoverable">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img class="activator" src="assets/images/restaurants/pizzahut1.jpg">
@@ -62,7 +91,7 @@
                     <div class="card-content">
                         <span class="card-title activator grey-text text-darken-4">Pizza Hut<i class="material-icons right">more_vert</i></span>
                         <p>
-                            A excelência em pizzas, lasanhas e massas em geral. Confira nosso cardápio.
+                            A excelência em pizzas, lasanhas e massas em geral. Confira!
                         </p>
                         <br>
                         <p class="center">
@@ -99,8 +128,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col m3">
-                <div class="card">
+            <div class="col l3 m3 s12">
+                <div class="card hoverable">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img class="activator" src="assets/images/restaurants/taipan.jpg">
                     </div>
@@ -144,7 +173,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col m3">
+            <div class="col l3 m3 s12">
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img class="activator" src="assets/images/restaurants/giraffas.jpg">
@@ -188,7 +217,51 @@
                     </div>
                 </div>
             </div>
-            <div class="col m3">
+            <div class="col l3 m3 s12">
+                <div class="card">
+                    <div class="card-image waves-effect waves-block waves-light">
+                        <img class="activator" src="assets/images/restaurants/giraffas.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title activator grey-text text-darken-4">Giraffas<i class="material-icons right">more_vert</i></span>
+                        <p>
+                            Semana especial! Na compra do GIRAKIDS você ganha um brinde!
+                        </p>
+                        <br>
+                        <p class="center">
+                            <i class="material-icons yellow-text ">star</i>
+                            <i class="material-icons yellow-text ">star</i>
+                            <i class="material-icons yellow-text ">star</i>
+                            <i class="material-icons yellow-text ">star</i>
+                        </p>
+                    </div>
+                    <div class="card-reveal">
+                        <span class="card-title grey-text text-darken-4">Pizza Hut<i class="material-icons right">close</i></span>
+                        <p>Confira nosso menu de massas completo. Além de pizzas doces especiais, sucos e muito mais.</p>
+                        <div class="row">
+                            <div class="col m12">
+                                <a href="#" class="btn white col m12 red-text text-darken-3"> Cardápio Completo </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col m12">
+                                Tempo médio: <span class="green-text flow-text"> 35 min</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col m12">
+                                Valor médio: <span class="green-text flow-text"> R$ 39,90</span>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col m12">
+                                Grau de Satisfação: <span class="green-text flow-text"> 86%</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col l3 m3 s12">
                 <div class="card">
                     <div class="card-image waves-effect waves-block waves-light">
                         <img class="activator" src="assets/images/restaurants/giraffas.jpg">
@@ -280,13 +353,6 @@
 <script src="assets/js/init.js"></script>
 <script>
     $(document).ready(function(){
-        $('.collapsible').collapsible({
-            accordion : true // A setting that changes the collapsible behavior to expandable instead of the default accordion style
-        });
-        $('.simulate').pushpin({
-            top: $('.simulate').offset().top,
-            offset: 100,
-        });
         $('select').material_select();
     });
 </script>
