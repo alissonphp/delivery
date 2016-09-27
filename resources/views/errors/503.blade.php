11c169b55d687bf9b4277ac56e5805f39b14b32c
Alisson Gomes <alisson.php@gmail.com>
2016-09-27 13:50:38 +0000

    503.blade.php alterados online com o Bitbucket

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta charset="utf-8">
         <title>Manutenção.</title>
 
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
